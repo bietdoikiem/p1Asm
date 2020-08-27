3f5f542c17c5a714df6556ede5fd0642d8177fba
CallMeDKay <s3812649@rmit.edu.vn>
2020-08-27 10:01:22 +0700

    Ignore again 27/08

--- src/com/RMIT/Main.java
+++ src/com/RMIT/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello a dat");
-        System.out.println("Ignore workspace.xml file");
+        System.out.println("Another ingore");
 	// write your code here
     }
 }
