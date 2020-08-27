8014e6ee6f2db92fc8aabb335c048b13a509a4ad
dat711 <datngonguyentien@gmail.com>
2020-08-27 18:05:37 +0700

    adding convert date, string function

--- src/com/RMIT/Main.java
+++ src/com/RMIT/Main.java
@@ -2,7 +2,7 @@ package com.RMIT;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("hello world");
         System.out.println("hello a dat");
         System.out.println("Ignore successfully");
