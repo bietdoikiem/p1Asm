bfa00f6e66d2203af0ef0af16cd81abac7d5608a
CallMeDKay <s3812649@rmit.edu.vn>
2020-08-27 09:46:22 +0700

    Main commit

--- src/com/RMIT/Main.java
+++ src/com/RMIT/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello a dat");
+        System.out.println("Create new branches");
 	// write your code here
     }
 }
