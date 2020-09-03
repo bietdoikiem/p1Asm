e101595dd589eae59ed83f50a7ea088ab44a397f
dat711 <datngonguyentien@gmail.com>
2020-09-03 10:13:34 +0700

    commit for merge

--- src/main/java/com/RMIT/Main.java
+++ src/main/java/com/RMIT/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         // call functions here
-        CSVManager csvmanager = CSVManager.getInstance();
-        csvmanager.print();
-        //Lead lead1 = new Lead("Minh", Converter.StrToDate("2001-08-24"), true, "0934717924", "minhlucky2408@gmail.com", "1002 Ta Quang Buu");
-        //csvmanager.writeFile();
-        //csvmanager.writeLead();
-        //csvmanager.readLead();
-        csvmanager.readLastId();
+//        CSVManager csvmanager = CSVManager.getInstance();
+//        csvmanager.print();
+//        //Lead lead1 = new Lead("Minh", Converter.StrToDate("2001-08-24"), true, "0934717924", "minhlucky2408@gmail.com", "1002 Ta Quang Buu");
+//        //csvmanager.writeFile();
+//        //csvmanager.writeLead();
+//        //csvmanager.readLead();
+//        csvmanager.readLastId();
     }
 }
